978465e3b2d3f529fea8f9c9834a37cfd895db9a
nasik_ananasik <borodina-nas@mail.ru>
2025-09-29 22:32:01 +0300

    fail test

---
+++ src/test/java/ru/netology/restAPI/Test_1.java
@@ -0,0 +1,36 @@
+package ru.netology.restAPI;
+
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class Test_1 {
+    @Test
+    void should() {
+        String body = "{\"id\": 1," +
+                "\"name\": \"Текущий счёт\"," +
+                "\"number\": \"** 8574\"," +
+                "\"balance\": 550000," +
+                "\"currency\": \"RUB\"" +
+                "}";
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("application/json")
+                .body(body) // отправляемые данные (заголовки и query можно выставлять аналогично)
+                // Выполняемые действия
+                .when()
+                .post("/post")
+                // Проверки
+                .then()
+                .statusCode(201)
+                .body("json.id", equalTo(1))
+                .body("json.name", equalTo("Текущий счёт"))
+                .body("json.number", equalTo("** 8574"))
+                .body("json.balance", equalTo(550000))
+                .body("json.currency", equalTo("RUB"))
+        ;
+    }
+}
+
